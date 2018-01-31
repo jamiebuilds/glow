2202fd794bc358ba6c3fba1d5cddce76b84949e0
Alex Rattray <rattrayalex@stripe.com>
2018-01-31 14:19:22 -0800

    Fix test to ignore timing

--- test.js
+++ test.js
@@ -7,5 +7,10 @@ test('runs without crashing', t => {
   const { status, stderr, stdout } = spawnSync('node', ['cli.js']);
   t.is(status, 0);
   t.is(stdout.toString(), '');
-  t.snapshot(stderr.toString());
+  const stderrWithoutTiming = stderr
+    .toString()
+    .split('\n')
+    .slice(0, -2)
+    .join('\n');
+  t.snapshot(stderrWithoutTiming);
 });
