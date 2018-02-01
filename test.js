62615a4015e725f81bd1e600f2965ea0c37aaf77
Alex Rattray <rattrayalex@stripe.com>
2018-01-31 21:58:59 -0800

    Use regex instead of snapshot test

--- test.js
+++ test.js
@@ -7,10 +7,5 @@ test('runs without crashing', t => {
   const { status, stderr, stdout } = spawnSync('node', ['cli.js']);
   t.is(status, 0);
   t.is(stdout.toString(), '');
-  const stderrWithoutTiming = stderr
-    .toString()
-    .split('\n')
-    .slice(0, -2)
-    .join('\n');
-  t.snapshot(stderrWithoutTiming);
+  t.regex(stderr.toString(), /Flow didn't find any errors/);
 });
